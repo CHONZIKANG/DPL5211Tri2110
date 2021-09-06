c0e04a76909458a92fe62a8f7428444bdd80b719
ZiKang <90165886+CHONZIKANG@users.noreply.github.com>
2021-09-06 10:49:02 +0800

    Add files via upload

---
+++ Lab 1.2.py
@@ -0,0 +1,10 @@
+number_1 = int(input("Please enter first number: "))
+number_2 = int(input ("Please enter second number: "))
+
+total = number_1 + number_2
+
+# print("Total number is :",total) #
+# The sum of 5 and 6 is 11 #
+
+# print("The sum of",number_1,"and",number_2,"is",total) #
+print("The sum of {} and {} is {}". format(number_1,number_2,total)) 
