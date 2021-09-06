847647c25c272ca7287de65f51088e4451bb5562
CHONZIKANG <90165886+CHONZIKANG@users.noreply.github.com>
2021-09-06 10:41:13 +0800

    Add files via upload

---
+++ Lab 1.1.py
@@ -0,0 +1,14 @@
+#Student ID: 1201200750 #
+#Student Name: Chon Zi Kang #
+# hashtag is comment (control and slash for )#
+
+print("I like Python")
+
+person = input("Please enter your name : ")
+print("Hello",person)
+
+
+
+
+
+
